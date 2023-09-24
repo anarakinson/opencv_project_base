1228f2c278f22a7c290082dbea69715fc9885eb8
anarakinson <anarakinson@gmail.com>
2023-09-24 10:53:47 +0300

    add some examples from opencv

--- src/test.cpp
+++ src/test.cpp
@@ -10,7 +10,7 @@ using namespace cv;
 int main() {
 
     // std::string image_path = samples::findFile("starry_night.jpg");
-    std::string image_path{"starry_night.jpg"};
+    std::string image_path{"src/starry_night.jpg"};
     Mat img = imread(image_path, IMREAD_COLOR);
 
     if (img.empty()) {
